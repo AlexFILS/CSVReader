eb7921b9b6f3c33d700a2df6f792077d829a86bf
Mihai Alexandru <mh.alex93@gmail.com>
2022-10-11 17:16:38 +0300

    - Initial setup

--- CSVReader_SB/CSVReader/HomePage/ViewControllers/HomeViewController.swift
+++ CSVReader_SB/CSVReader/HomePage/ViewControllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CSVReader_SB
 //
 //  Created by Alexandru Mihai on 11.10.2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
